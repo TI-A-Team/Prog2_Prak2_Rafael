12b3e39c22fe28cd117cd395168c20b0b9564548
RGrabolus <ra.grabolus@gmail.com>
2026-04-14 18:59:10 +0200

    Implement Halbaddierer class with logic gates

--- halbaddierer.h
+++ halbaddierer.h
@@ -1,6 +1,33 @@
+#ifndef HALBADDIERER_H_
+#define HALBADDIERER_H_
+
 #include "schaltung.h"
+
 
 class Halbaddierer : public Schaltung
 {
 
+	Halbaddierer(Schnittstelle* e0, Schnittstelle* e1, Schnittstelle* a0, Schnittstelle* a1)
+	{
+		this->Eingang.push_back(e0);
+		this->Eingang.push_back(e1);
+		this->Ausgang.push_back(a0);
+		this->Ausgang.push_back(a1);
+
+		Xor2* xor2 = new Xor2(e0, e1, a0);
+		this->bausteine.push_back(xor2);
+
+		Und2* und2 = new Und2(e0, e1, a1);
+		this->bausteine.push_back(und2);
+
+		this->name = "Halbaddierer";
+	}
+
+	void update()
+	{
+		this->bausteine[0]->update();
+		this->bausteine[1]->update();
+	}
 };
+
+#endif
