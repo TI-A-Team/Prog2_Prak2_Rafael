2a5a0a9a5451b6bcd525d261829b5427b9eef33c
RGrabolus <ra.grabolus@gmail.com>
2026-04-14 19:21:49 +0200

    Uncomment and update includes and test function

--- Prog2_Prak2.cpp
+++ Prog2_Prak2.cpp
@@ -3,9 +3,9 @@
 #include <string>
 
 //#include "schnittstelle.h"
-//#include "baustein.h"
+#include "baustein.h"
 //#include "not1.h"
-//#include "oder2.h"
+#include "oder2.h"
 //#include "und2.h"
 //#include "xor2.h"
 //#include "schaltung.h"
@@ -18,12 +18,38 @@
 
 void test2(Baustein *b)
 {
+	b->Eingang[0]->setPegel(0);
+	b->Eingang[1]->setPegel(0);
+	b->update();
+	b->print();
 
+	b->Eingang[0]->setPegel(0);
+	b->Eingang[1]->setPegel(1);
+	b->update();
+	b->print();
+
+	b->Eingang[0]->setPegel(1);
+	b->Eingang[1]->setPegel(0);
+	b->update();
+	b->print();
+
+	b->Eingang[0]->setPegel(1);
+	b->Eingang[1]->setPegel(1);
+	b->update();
+	b->print();
 }
 
 
 int main()
 {
+	Schnittstelle e0;
+	Schnittstelle e1;
+	Schnittstelle e2;
+	Schnittstelle a0;
+	Schnittstelle a1;
+
+	Oder2 oder2(&e0, &e1, &a0);
+	test2(&oder2);
 
 	return 0;
 }
