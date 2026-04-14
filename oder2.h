5b7ee2a5731c8f52926d6425e013493c61cddf32
RGrabolus <ra.grabolus@gmail.com>
2026-04-14 18:58:30 +0200

    Update oder2.h

--- oder2.h
+++ oder2.h
@@ -1,25 +1,28 @@
+#ifndef ODER2_H_
+#define ODER2_H_
+
 #include "baustein.h"
-using namespace std;
 
 class Oder2 : public Baustein
 {
+	public:
+
 	Oder2(Schnittstelle* e0, Schnittstelle* e1, Schnittstelle* a0)
 	{
 		Eingang.push_back(e0);
 		Eingang.push_back(e1);
 		Ausgang.push_back(a0);
-
 		name = "Oder2";
 	}
 
-	public:
+
 	void update()
 	{
-		if (Eingang[0] == Schnittstelle::HIGH || Eingang[1] == Schnittstelle::HIGH) Ausgang[0] = Schnittstelle::HIGH; break;
-
-		if (Eingang[0] == Schnittstelle::UNDEFINED || Eingang[1] == Schnittstelle::UNDEFINED) Ausgang[0] = Schnittstelle::UNDEFINED; break;
-
-		Ausgang[0] = Schnittstelle::LOW;
+		if (Eingang[0]->getPegel() == -1 || Eingang[1]->getPegel() == -1) Ausgang[0]->setPegel(-1); break;
+		if (Eingang[0]->getPegel() == 1 || Eingang[1]->getPegel() == 1) Ausgang[0]->setPegel(1); break;
+			Ausgang[0]->setPegel(0);
 	}
 
-}
+};
+
+#endif
