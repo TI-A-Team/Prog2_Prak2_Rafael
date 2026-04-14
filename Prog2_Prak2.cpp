6b50410fff63bc33c49a4d4fcb741ee64d05468a
RGrabolus <ra.grabolus@gmail.com>
2026-04-14 18:57:07 +0200

    Refactor includes and add test2 function
    Updated includes and added test2 function.

--- Prog2_Prak2.cpp
+++ Prog2_Prak2.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
-#include <string>
 #include <vector>
-using namespace std;
+#include <string>
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+//#include "schnittstelle.h"
+//#include "baustein.h"
+//#include "not1.h"
+//#include "oder2.h"
+//#include "und2.h"
+//#include "xor2.h"
+//#include "schaltung.h"
+//#include "halbaddierer.h"
+//#include "volladdierer.h"
 
-	vector<string> Eingang;
+
+
+//using namespace std;
+
+void test2(Baustein *b)
+{
+
+}
+
+
+int main()
+{
+
 	return 0;
 }
