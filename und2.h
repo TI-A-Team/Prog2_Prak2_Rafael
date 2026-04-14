8ec288383c8d8099d9287738d145c4328d01c45c
RGrabolus <ra.grabolus@gmail.com>
2026-04-14 18:58:20 +0200

    Refactor Und2 class update method logic

--- und2.h
+++ und2.h
@@ -1,25 +1,26 @@
+#ifndef UND2_H_
+#define UND2_H_
+
 #include "baustein.h"
-using namespace std;
 
 class Und2 : public Baustein
 {
+	public:
+
 	Und2(Schnittstelle* e0, Schnittstelle* e1, Schnittstelle* a0)
 	{
 		Eingang.push_back(e0);
 		Eingang.push_back(e1);
 		Ausgang.push_back(a0);
-
 		name = "Und2";
 	}
 
-	public:
 	void update()
 	{
-		if (Eingang[0] == Schnittstelle::HIGH && Eingang[1] == Schnittstelle::HIGH) Ausgang[0] = Schnittstelle::HIGH; break;
-
-		if (Eingang[0] == Schnittstelle::UNDEFINED || Eingang[1] == Schnittstelle::UNDEFINED) Ausgang[0] = Schnittstelle::UNDEFINED; break;
-
-		Ausgang[0] = Schnittstelle::LOW;
+		if (Eingang[0]->getPegel() == -1 || Eingang[1]->getPegel() == -1) Ausgang[0]->setPegel(-1); break;
+		if (Eingang[0]->getPegel() == 0 || Eingang[1]->getPegel() == 0) Ausgang[0]->setPegel(0); break;
+			Ausgang[0]->setPegel(1);
 	}
+};
 
-}
+#endif
